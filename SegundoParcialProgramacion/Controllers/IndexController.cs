c986de34e4b728c25a5e1431e8380fbcc58b5dc2
diegoazh <diegoazh2003@gmail.com>
2016-06-28 14:00:45 -0300

    Segundo parcial de programacion terminado

---
+++ SegundoParcialProgramacion/Controllers/IndexController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SegundoParcialProgramacion.Models;
+
+namespace SegundoParcialProgramacion.Controllers
+{
+    public class IndexController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            comentarioDataContext db = new comentarioDataContext();
+            var comentarios = 
+                from coment in db.Comentarios
+                orderby coment.fecha descending
+                select coment;
+            ViewBag.comentarios = comentarios;
+
+            return View(ViewBag.comentarios);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string nombre, string comentario)
+        {
+            comentarioDataContext db = new comentarioDataContext();
+            Comentario c = new Comentario();
+            if(nombre.Length > 4)
+                c.nombre = nombre;
+            if(comentario.Length != 0)
+                c.comentario1 = comentario;
+
+            c.fecha = DateTime.Today;
+            db.Comentarios.InsertOnSubmit(c);
+            db.SubmitChanges();
+
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ SegundoParcialProgramacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SegundoParcialProgramacion.MvcApplication" Language="C#" %>
